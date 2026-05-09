cc1a94ded553ef4104cdc50592f12a792aa1170d
Artem <artemon4xxl@gmail.com>
2026-05-09 20:59:03 +0300

    Добавьте файлы проекта.

---
+++ Rail/main.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/Module.h>
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/Support/raw_ostream.h>
+
+int main() {
+    std::cout << "Hello rail!" << std::endl;
+}
